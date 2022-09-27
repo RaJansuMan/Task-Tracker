cb96d2a5413aaafa135e946d67bc631ab9690056
RaJansuMan <rajansuman180@gmail.com>
2022-09-27 14:35:46 +0530

    comment deleted

--- src/component/Addtask.js
+++ src/component/Addtask.js
@@ -1,50 +1,51 @@
 import React from 'react'
-import {useState} from'react'
+import { useState } from 'react'
 
 
 
-const Addtask = ({onAdd}) => {
-    const [text,setText]=useState('')
-    // let [day,setDay]=useState('')
-    const [reminder,setReminder]=useState(false)
-     
-        
-    const onSubmit=(e)=>{
-        e.preventDefault()
+const Addtask = ({ onAdd }) => {
+  const [text, setText] = useState('')
+  // let [day,setDay]=useState('')
+  const [reminder, setReminder] = useState(false)
 
-        if(!text){
-        alert('Please add a task')
-        return}
-      
-        let day=''
-        {
-          const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-          let myDate=new Date();
-          let date=months[myDate.getMonth()]+' '+myDate.getDate()+'th'
-          let d=myDate.getHours()
-          let m=myDate.getMinutes()
-          let time=(d>12?(d-12):d)+':'+(m>9?m:('0'+m))+' '+(d>=12?'pm':'am')
-           day=date+' at '+time
-          }
-          // setDay(day)
-          console.log(day)
-          //console.log(newDate)
-        onAdd({text,day,reminder})
 
-        setText('')
-        // setDay('')
-        setReminder(false)
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (!text) {
+      alert('Please add a task')
+      return
     }
+
+    let day = ''
+    {
+      const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+      let myDate = new Date();
+      let date = months[myDate.getMonth()] + ' ' + myDate.getDate() + 'th'
+      let d = myDate.getHours()
+      let m = myDate.getMinutes()
+      let time = (d > 12 ? (d - 12) : d) + ':' + (m > 9 ? m : ('0' + m)) + ' ' + (d >= 12 ? 'pm' : 'am')
+      day = date + ' at ' + time
+    }
+
+    console.log(day)
+
+    onAdd({ text, day, reminder })
+
+    setText('')
+    // setDay('')
+    setReminder(false)
+  }
 
   return (
     <form className='add-form' onSubmit={onSubmit}>
-        <div className='form-control'>
-          <label>Task</label>
-          <input type='text' placeholder='Add Task'
+      <div className='form-control'>
+        <label>Task</label>
+        <input type='text' placeholder='Add Task'
           value={text}
-          onChange={(e)=>setText(e.target.value)}
-          />
-        </div>
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
       {/*<div className='form-control'>
           <label>Date & Time</label>
           <input type='text' placeholder='Add Day & Time'
@@ -52,15 +53,15 @@ const Addtask = ({onAdd}) => {
           onChange={(e)=>setDay(e.target.value)}
            />
   </div>*/}
-        <div className='form-control form-control-check'>
-          <label>Set Reminder</label>
-          <input type='checkbox'
+      <div className='form-control form-control-check'>
+        <label>Set Reminder</label>
+        <input type='checkbox'
           checked={reminder}
-           value={reminder}
-          onChange={(e)=>setReminder(e.currentTarget.checked)} 
-          />
-        </div>
-        <input type='submit' value='Save Task' className='btn btn-block'/>
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
+      </div>
+      <input type='submit' value='Save Task' className='btn btn-block' />
     </form>
   )
 }
